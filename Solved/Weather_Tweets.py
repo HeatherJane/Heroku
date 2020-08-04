4bfa2ae000a77b757bad3a12953b9505359854da
HeatherJane <heatherjane.moore@gmail.com>
2020-08-03 20:04:12 -0400

    test

---
+++ Solved/Weather_Tweets.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Dependencies
+import tweepy
+import time
+import json
+import os
+import random
+import requests as req
+import datetime
+from config import consumer_key, consumer_secret, access_token, access_token_secret, weather_api_key
+
+
+# In[3]:
+
+
+# Twitter API Keys
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+
+# In[4]:
+
+
+# Create a function that gets the weather in London and Tweets it
+def WeatherTweet():
+
+    # Construct a Query URL for the OpenWeatherMap
+    url = 'http://api.openweathermap.org/data/2.5/weather?'
+    city = 'Washington, D.C.'
+    units = 'imperial'
+    query_url = (f'{url}appid={weather_api_key}&q={city}&units={units}')
+
+    # Perform the API call to get the weather
+    weather_response = req.get(query_url)
+    weather_json = weather_response.json()
+    print(weather_json)
+
+    # Twitter credentials
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    # Tweet the weather
+    api.update_status(
+        "[Heather] - Weather in DC " +\
+        (datetime.datetime.now().strftime("%I:%M %p") + " " +\
+         str(weather_json["main"]["temp"])+"F"))
+
+    # Print success message
+    print("Tweeted successfully!")
+
+
+# In[ ]:
+
+
+# Test out the function
+# WeatherTweet()
+
+
+# In[5]:
+
+
+# Tweet out the weather every one minute
+while(True):
+    WeatherTweet()
+    time.sleep(60)
+
+
+# In[ ]:
+
+
+
+
